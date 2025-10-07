6d04b1805908d010be1631e1544e39bdbbd1595c
Piyush More <piyushmore109@gmail.com>
2025-10-07 23:02:43 +0530

    DLL

---
+++ LinkedList/DLL/DLLBasic.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	value int
+	next  *Node
+	prev  *Node
+}
+
+type LinkedList struct {
+	head *Node
+}
+
+func (l *LinkedList) InsertAtEnd(value int) {
+	newNode := &Node{value: value}
+
+	if l.head == nil {
+		l.head = newNode
+		return
+	}
+
+	current := l.head
+	for current.next != nil {
+		current = current.next
+	}
+
+	current.next = newNode
+	newNode.prev = current
+}
+
+func (l *LinkedList) Print() {
+	current := l.head
+	for current != nil {
+		fmt.Printf("%d -> ", current.value)
+		current = current.next
+	}
+	fmt.Println("nil")
+}
+
+func main() {
+	list := LinkedList{}
+	list.InsertAtEnd(10)
+	list.InsertAtEnd(20)
+	list.InsertAtEnd(30)
+	list.Print()
+}
